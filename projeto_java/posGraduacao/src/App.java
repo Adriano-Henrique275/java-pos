71d92a2257152bdaa678e8cb4635b501e7338355
Adriano Henrique <adriano.henrique275@gmail.com>
2023-08-20 10:32:30 -0300

    feat: refactory code

--- projeto_java/posGraduacao/src/App.java
+++ projeto_java/posGraduacao/src/App.java
@@ -8,19 +8,36 @@ public class App {
         * Mostre o maior, menor, media e o vetor  
         */
 
-        float numeros[] = new float[3];
 
-        for (int i = 0; i < 3; i++) {
-            numeros[i] = Float.parseFloat(JOptionPane.showInputDialog("Digite um valor"));
+        int tamanho = 10;
+        float valores [] = new float[tamanho];
+
+        for (int i = 0; i < tamanho; i++) {
+            valores[i] = Float.parseFloat(JOptionPane.showInputDialog("Digite um valor" + i));
         }
 
-        String result = "";
+        float maior = valores[0];
+        float menor = valores[0];
+        float media = valores[0];
+        String reusltado = "" + valores[0] + "\n";
 
-        for (float aux : numeros ) {
-            System.out.println("teste" + aux);
-            result += aux + "\n";
+        for(int i = 0; i < tamanho; i++) {
+            if(maior < valores[i]) {
+                maior = valores[0];
+            }
+
+            if(menor < valores[i]) {
+                menor = valores[0];
+            }
+            
+            media += valores[i];
+            reusltado += valores[i] + "\n";
         }
-        
-        JOptionPane.showMessageDialog(null, result);
+
+        media /= tamanho;
+
+        reusltado = "Media: " + media + "\n Maior: " + maior + "\n Menor: " + menor + " \n Vetor completo: \n" + reusltado;
+        JOptionPane.showMessageDialog(null, reusltado);
+
     }
 }
